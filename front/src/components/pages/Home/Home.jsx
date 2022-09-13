b098f6c5c0671043a6115be1cda6caec9bb78250
Mikhail <msl2002@yandex.ru>
2022-09-13 17:45:02 +0300

    scss, header, add styles variables

---
+++ front/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../common/Header/Header'
+
+const Home = () => {
+	return (
+		<>
+			<div>Home page</div>
+		</>
+	)
+}
+
+export default Home
--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './sass/index.scss'
-import App from './App';
+import ReactDOM from 'react-dom';
+import './scss/index.scss'
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
